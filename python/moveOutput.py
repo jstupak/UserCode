99c4c35bc65d78d9cdb8df1cda60eebecc4bed19
jstupak <>
2013-02-13 19:21:32 +0000

    error detection bugfix

--- python/moveOutput.py
+++ python/moveOutput.py
@@ -9,13 +9,13 @@ except:
     exit()
 
 for sample in theSamples:
-    nJobs=len(glob(destination+'/'+sample.name+'_*.job'))
+    nJobs=len(glob(destination+'/'+sample.name+'/'+sample.name+'_*.job'))
     nSuccesful=len(glob(sample.name+'_*.root'))
 
     if nSuccesful<nJobs: print 40*'!','\nProblem with sample:',sample.name,'\n'
     
     command='mv '+sample.name+'* '+destination+'/'+sample.name+'/'
-    #print command
-    #system(command)
+    print command
+    system(command)
 
     
